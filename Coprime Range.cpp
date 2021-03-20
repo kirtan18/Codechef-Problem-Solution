7d6dd26fc1123007ee4471d4aeb9054e6f86cf54
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-03-21 00:02:59 +0530

    Create Coprime Range.cpp

---
+++ Coprime Range.cpp
@@ -0,0 +1,18 @@
+
+signed main(){
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+     
+    int T;
+    cin >> T;
+    while(T--){
+       int n ,k;
+       cin>>n >>k;
+       cout<<1000000007<<endl;
+    }
+       
+       
+     return 0;
+}
