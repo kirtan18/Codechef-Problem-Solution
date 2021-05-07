eda0bc16a14f8116e2d05811fec524b7f9a2da65
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-05-07 23:13:40 +0530

    Create Golf.cpp

---
+++ Golf.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/MAY21C/problems/LKDNGOLF
+
+
+
+
+
+
+void solve() {
+
+    lli n,x,k;
+    cin >> n >> x >> k;
+    
+    lli tmp = (n+1)%k;
+    if(x%k == 0 || x%k == tmp){
+        cout <<"YES" <<"\n";
+    }else{
+        cout <<"NO" <<"\n";
+    }
+
+}
