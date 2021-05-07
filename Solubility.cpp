3785f7e2abafda054afeb150b682d4af72f36664
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-05-07 23:12:54 +0530

    Create Solubility.cpp

---
+++ Solubility.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/MAY21C/problems/SOLBLTY
+
+
+
+
+
+void solve() {
+
+    int x,a,b;
+    cin >> x >> a >> b;
+
+    int ans = a + (100 - x)*b;
+
+    ans = ans * 10;
+
+    cout << ans <<"\n";
+  
+}
+
+
